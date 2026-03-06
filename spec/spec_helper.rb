173bc3f7b13bcd647a21151233404e578d8e5a1d
Konstantin Gredeskoul <kigster@gmail.com>
2026-03-05 21:01:22 -0800

    Fixing rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@
 require "simplecov"
 
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-SimpleCov.start do 
-  add_filter /spec/
+SimpleCov.start do
+  add_filter(/spec/)
 end
 
 require "flowengine"
